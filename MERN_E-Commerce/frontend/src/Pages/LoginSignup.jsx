6beb518874c5a0fbdea9abc82f2c11d3492e3e0d
Suraj kumar <surajkumar95931@gmail.com>
2024-04-08 22:54:14 +0530

    final push project completion.

--- MERN_E-Commerce/frontend/src/Pages/LoginSignup.jsx
+++ MERN_E-Commerce/frontend/src/Pages/LoginSignup.jsx
@@ -1,23 +1,127 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CSS/LoginSignup.css";
 const LoginSignup = () => {
+  const [state, setState] = useState("Login");
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    email: "",
+  });
+  const changeHandler = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const login = async () => {
+    console.log("login function executed", formData);
+    let responseData;
+    await fetch("http://localhost:4000/login", {
+      method: "POST",
+      headers: {
+        Accept: "application/form-data",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      // .then((response) => response.json())
+      // .then((data) => (responseData = data));
+      .then((response) => response.json())
+      .then((data) => (responseData = data));
+    if (responseData.success) {
+      localStorage.setItem("auth-token", responseData.token);
+      window.location.replace("/");
+    } else {
+      alert(responseData.errors);
+    }
+  };
+
+  const Signup = async () => {
+    console.log("Signup function executed", formData);
+    let responseData;
+    await fetch("http://localhost:4000/signup", {
+      method: "POST",
+      headers: {
+        Accept: "application/form-data",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => (responseData = data));
+
+    if (responseData.success) {
+      localStorage.setItem("auth-token", responseData.token);
+      window.location.replace("/");
+    } else {
+      alert(responseData.errors);
+    }
+  };
+
   return (
     <div className="loginsignup">
       <div className="loginsignup-container">
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className="loginsignup-fields">
-          <input type="text" placeholder="your name" />
+          {state === "Sign Up" ? (
+            <input
+              name="username"
+              value={formData.username}
+              onChange={changeHandler}
+              type="text"
+              placeholder="YOUR NAME"
+            />
+          ) : (
+            <></>
+          )}
 
-          <input type="email" placeholder="your email" />
-          <input type="password" placeholder="Password" />
+          <input
+            name="email"
+            value={formData.email}
+            onChange={changeHandler}
+            type="email"
+            placeholder="YOUR EMAIL"
+          />
+          <input
+            name="password"
+            value={formData.password}
+            onChange={changeHandler}
+            type="password"
+            placeholder="YOUR PASSWORD"
+          />
         </div>
-        <button>Continue</button>
-        <p className="loginsignup-login">
-          Already have an account?<span>Login here</span>{" "}
-        </p>
+        <button
+          onClick={() => {
+            state === "Login" ? login() : Signup();
+          }}
+        >
+          Continue
+        </button>
+        {state === "Sign Up" ? (
+          <p className="loginsignup-login">
+            Already have an account?
+            <span
+              onClick={() => {
+                setState("Login");
+              }}
+            >
+              Login here
+            </span>
+          </p>
+        ) : (
+          <p className="loginsignup-login">
+            Create an account?
+            <span
+              onClick={() => {
+                setState("Sign Up");
+              }}
+            >
+              Click here
+            </span>
+          </p>
+        )}
+
         <div className="loginsignup-agree">
           <input type="checkbox" name="" id="" />
-          <p>By continuing ,i agree to terms of use and </p>
+          <p>By continuing ,i agree to terms & conditions </p>
         </div>
       </div>
     </div>
