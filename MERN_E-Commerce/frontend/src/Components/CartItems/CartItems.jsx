d4405d0749fdc2f142ab973da7aead06faec2c78
Suraj kumar <surajkumar95931@gmail.com>
2024-04-17 15:45:12 +0530

    commit new

--- MERN_E-Commerce/frontend/src/Components/CartItems/CartItems.jsx
+++ MERN_E-Commerce/frontend/src/Components/CartItems/CartItems.jsx
@@ -21,7 +21,7 @@ const CartItems = () => {
         if (cartItems[e.id] > 0) {
           return (
             <div>
-              <div className="cartitems-format cartitems-format-main">
+              <div className="cartitems-format  cartitems-format-main">
                 <img src={e.image} className="carticon-product-icon" alt="" />
                 <p>{e.name}</p>
                 <p>₹{e.new_price}</p>
@@ -63,7 +63,7 @@ const CartItems = () => {
               <h3>₹{getTotalCartAmount()}</h3>
             </div>
           </div>
-          <button>PROCEED to CHECKOUT</button>
+          <button>PROCEED TO CHECKOUT</button>
         </div>
         <div className="cartitems-promocode">
           <p>if you have a promocode enter here</p>
--- MERN_E-Commerce/frontend/src/Components/Footer/Footer.jsx
+++ MERN_E-Commerce/frontend/src/Components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
     <div className="footer">
       <div className="footer-logo">
         <img src={footer_logo} alt=""></img>
-        <p>CLOTHNEXT</p>
+        <p>SHOPPER</p>
       </div>
       <ul className="footer-links">
         <li>Company</li>
@@ -31,7 +31,7 @@ const Footer = () => {
       </div>
       <div className="footer-copyright">
         <hr />
-        <p>Copyright @2024 - ALL Rights Reserved </p>
+        <p>Copyright @2024 - ALL Rights Reserved</p>
       </div>
     </div>
   );
